da223e53d69faf36384e92c201e4ec5ec6d854b4
Will Train <87541128+wt-314159@users.noreply.github.com>
2025-03-24 12:46:17 +0000

    completing day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,10 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::{collections::HashSet, fmt::Display, num::ParseIntError, str::FromStr};
 
 fn main() {
     let input = include_str!("../inputs/puzzle_input.txt");
+    // let input = include_str!("../inputs/test_puzzle_input.txt");
 
-    part_one(input);
+    part_two(input);
 }
 
 #[allow(dead_code)]
@@ -11,13 +12,36 @@ fn part_one(input: &str) {
     let coords = get_all_coords(input);
     let max_x = coords.iter().map(|c| c.x).max().unwrap();
     let max_y = coords.iter().map(|c| c.y).max().unwrap();
-    let mut grid = Grid::new(max_x + 5, max_y + 5);
+    let mut grid = Grid::new(max_x + 2, max_y + 2);
     grid.find_nearest_coords(&coords);
     eprintln!("Found all nearest coordinates");
+    let infinite = dbg!(grid.find_infinite_areas());
+    let areas = dbg!(grid.find_areas());
+
+    // eprintln!("{grid}");
+    let max_area = areas
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| !infinite.contains(&(*i as u8)))
+        .max_by(|(_, a1), (_, a2)| a1.cmp(a2))
+        .unwrap();
+
+    println!(
+        "Coord with max area has id: {}, and area: {}",
+        max_area.0, max_area.1
+    );
 }
 
 #[allow(dead_code)]
-fn part_two(_input: &str) {}
+fn part_two(input: &str) {
+    let coords = get_all_coords(input);
+    let max_x = coords.iter().map(|c| c.x).max().unwrap();
+    let max_y = coords.iter().map(|c| c.y).max().unwrap();
+    let grid = Grid::new(max_x + 2, max_y + 2);
+
+    let safe_area_size = grid.find_size_of_safe_region(&coords);
+    println!("Size of safe region: {safe_area_size}");
+}
 
 fn get_all_coords(input: &str) -> Vec<Coord> {
     input
@@ -31,9 +55,27 @@ fn get_all_coords(input: &str) -> Vec<Coord> {
         .collect()
 }
 
+#[derive(Debug, Clone)]
 struct Grid {
     grid: Box<[u8]>,
     stride: usize,
+}
+
+impl Display for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in self.grid.chunks_exact(self.stride) {
+            for id in row {
+                if *id == 0 {
+                    write!(f, ".")?;
+                } else {
+                    // write!(f, "{id:_>2},")?
+                    write!(f, "{id}")?
+                }
+            }
+            writeln!(f, "|")?;
+        }
+        Ok(())
+    }
 }
 
 impl Grid {
@@ -45,22 +87,82 @@ impl Grid {
         }
     }
 
+    fn find_size_of_safe_region(&self, coords: &[Coord]) -> usize {
+        let mut size = 0;
+        let (mut x, mut y) = (0, 0);
+        self.grid.iter().for_each(|_| {
+            let total_manhattan: usize = coords.iter().map(|c| c.manhattan_distance((x, y))).sum();
+            if total_manhattan < 10_000 {
+                size += 1;
+            }
+            x += 1;
+            if x >= self.stride {
+                y += 1;
+                x = 0;
+            }
+        });
+        size
+    }
+
+    fn find_infinite_areas(&self) -> HashSet<u8> {
+        // All ids on the borders will extend infitely
+        let mut infinite = HashSet::new();
+        self.grid.iter().take(self.stride).for_each(|id| {
+            infinite.insert(*id);
+        });
+        self.grid
+            .iter()
+            .skip(self.stride)
+            .step_by(self.stride)
+            .for_each(|id| {
+                infinite.insert(*id);
+            });
+        self.grid
+            .iter()
+            .skip(self.stride - 1)
+            .step_by(self.stride)
+            .for_each(|id| {
+                infinite.insert(*id);
+            });
+        self.grid.iter().rev().take(self.stride).for_each(|id| {
+            infinite.insert(*id);
+        });
+        infinite
+    }
+
     fn find_nearest_coords(&mut self, coords: &[Coord]) {
-        let (mut row, mut col) = (0, 0);
+        let (mut x, mut y) = (0, 0);
         for n in self.grid.iter_mut() {
-            *n = coords
+            let mut smallest = usize::MAX - 1;
+            let mut second_smallest = usize::MAX;
+            let mut smallest_id = 0;
+            coords
                 .iter()
-                .map(|c| (c.manhattan_distance((row, col)), c))
-                .min_by(|(d1, _), (d2, _)| d1.cmp(d2))
-                .unwrap()
-                .1
-                .id;
-            col += 1;
-            if col >= self.stride {
-                row += 1;
-                col = 0;
+                .map(|c| (c.manhattan_distance((x, y)), c))
+                .for_each(|(d, c)| {
+                    if d < smallest {
+                        second_smallest = smallest;
+                        smallest = d;
+                        smallest_id = c.id;
+                    } else if d < second_smallest {
+                        second_smallest = d;
+                    }
+                });
+            if smallest != second_smallest {
+                *n = smallest_id;
+            }
+            x += 1;
+            if x >= self.stride {
+                y += 1;
+                x = 0;
             }
         }
+    }
+
+    fn find_areas(&self) -> [i32; 51] {
+        let mut areas = [0; 51];
+        self.grid.iter().for_each(|id| areas[usize::from(*id)] += 1);
+        areas
     }
 }
 
